2fead8671ee6ba0532d4118ff47341144378cc9b
Samuel Courtney <sacourtney@crimson.ua.edu>
2019-05-10 23:05:08 -0500

    Updates

---
+++ precache-manifest.fbf8f9bdc21282bcda9ee6c60e872b90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d90d841d45025de52ef1aca19e54a3",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "dd2ada841322ef3e8f25",
+    "url": "/website/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "02bc645764683e127966",
+    "url": "/website/static/js/2.70f6c4d2.chunk.js"
+  },
+  {
+    "revision": "dd2ada841322ef3e8f25",
+    "url": "/website/static/js/main.dd912e89.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec752809d24a76226aa377c9dd45133f.js"
+  "/website/precache-manifest.fbf8f9bdc21282bcda9ee6c60e872b90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
