a62d6540252532102d653cbf0fd50e9de8f3f0d9
Samuel Courtney <sacourtney@crimson.ua.edu>
2020-07-28 13:34:23 -0500

    Updates

---
+++ precache-manifest.d756ee1989182274a1e9e6e2928c06df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb80d2a9480265fb519dcf282c75f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35fc4ac0056341447154",
+    "url": "/static/css/main.45d2b714.chunk.css"
+  },
+  {
+    "revision": "8c06c325f6951f8ca561",
+    "url": "/static/js/2.239cfdf9.chunk.js"
+  },
+  {
+    "revision": "35fc4ac0056341447154",
+    "url": "/static/js/main.0497ca7c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d8ad28387a1546aa9f9bece1f57718b",
+    "url": "/static/media/SClogo.5d8ad283.png"
+  },
+  {
+    "revision": "8b1a9caa51072ef635b9f372b4835c2f",
+    "url": "/static/media/backgroundr.8b1a9caa.jpg"
+  }
+]);
