52742be34b09120c8f0eaddb797c93a225dd57f5
samuel courteny <50221874+samuelcourtney@users.noreply.github.com>
2021-09-25 10:39:59 -0500

    Updates

---
+++ precache-manifest.d441e5eda6a1272649bf7aa9f89f39ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3b92f95b4c8be7f573b06dbd19f16b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99f104eded29ec4be86c",
+    "url": "/static/css/main.1c70a8d0.chunk.css"
+  },
+  {
+    "revision": "fdc0791de752cbc39b8c",
+    "url": "/static/js/2.136cffb2.chunk.js"
+  },
+  {
+    "revision": "99f104eded29ec4be86c",
+    "url": "/static/js/main.c4e6d8bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef24bf218f0552f150939829455d37cf",
+    "url": "/static/media/IMG_0001.ef24bf21.jpeg"
+  },
+  {
+    "revision": "ad0916ba7047ec008fc07f8a9461f514",
+    "url": "/static/media/IMG_0222.ad0916ba.jpg"
+  },
+  {
+    "revision": "5d8ad28387a1546aa9f9bece1f57718b",
+    "url": "/static/media/SClogo.5d8ad283.png"
+  },
+  {
+    "revision": "934be93596ef3d3e335db3c08294a739",
+    "url": "/static/media/formula.934be935.jpg"
+  },
+  {
+    "revision": "10d9311937f2a542aa171f6cedb9fda2",
+    "url": "/static/media/longboi.10d93119.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8802ff8812a4ff5dbcdec6b30944540.js"
+  "/precache-manifest.d441e5eda6a1272649bf7aa9f89f39ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
