4bd6b0a195adf286f6a63a0d6e1d503759f092be
samuel courteny <50221874+samuelcourtney@users.noreply.github.com>
2022-03-28 02:27:38 -0500

    commits baby

--- src/Containers/about.js
+++ src/Containers/about.js
@@ -5,7 +5,8 @@ import { Jumbotron as Jumbo, Image} from "react-bootstrap";
 import styled from 'styled-components';
 import blackBackground from '../src_assets/IMG_0001.jpeg';
 import './about.css';
-
+import degreepdf from '../src_assets/SamuelCourtney.pdf';
+import resumepdf from '../src_assets/scr.pdf'
 
 const Styles = styled.div`
 
@@ -31,42 +32,164 @@ const Styles = styled.div`
 
 }
 
+.jumbo {
+    color: white;
+    margin-bottom: 0;
+    background-color: black;
+    position: relative;
+    z-index: 9999;
+    background-color: black;
+    background-size: cover;
+
+}
+
+make_it_black_h1{
+    color: black;
+}
+
+h5{
+    color: white;
+}
+
+h4{
+    color: 
+}
+
 
 `;
 
 export const about = () => (
     <React.Fragment>
-            <Container>
+
+            <Container className="rellax" data-rellax-speed="30" data-rellax-mobile-speed="40">
             <Styles>
-                <Jumbotron>
+                    <div>
+                    <h4 align = 'left'>pull up, or scroll down... personally im a linux shell and macOS guy - but I digress.</h4>
+                    <h1 align = 'center'>⇣⇣⇣</h1>
+    <Styles>
+    <Jumbotron className="rellax" data-rellax-speed="10" z>
+    <Container>
+    <h1>🌔</h1>
+    <h1 align = 'right'>🛰</h1>
+    </Container>
+    </Jumbotron>
+    </Styles>
+
+                    { /*build the wall, we will make mexico pay for it */}
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
                     
+
+
+
                     <h1 align='bottom left'>>> samuelcourtney$ would like you to know...</h1> 
-                </Jumbotron>
-                
-                <Jumbotron>
+               
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+ 
+                    
                     <h1>I am a creative yet critical thinker that really enjoys data engineering as a whole 😎</h1>
                     <br></br>
-                    <h3>ECE Major from The University of Alabama - Roll Tide! 🐘</h3>
-                    (summa cum laude: 3.961/4.0 GPA)
+
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+                    <h3><t>ECE Major from The University of Alabama - Roll Tide! 🐘</t></h3>
+                    <h5>(summa cum laude: 3.961/4.0 GPA)</h5>
+
+    <Styles>
+    <Jumbotron className="rellax" data-rellax-speed="-1" z>
+    <Container>
+    <h4>🎓 In college, I had one too many... </h4>
+    <h4 align = 'right'>>>SEG_FAULTS and CORE_DUMPS</h4>
+    </Container>
+    </Jumbotron>
+    </Styles>
                     <br></br>
                     <br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
                     <h3>I'm 24 years young</h3>
                     <br></br>
                     <h3>I have worked in IT for a while, doing Co-op and internships @ JobCost Inc., United Airlines, GE Appliances, and worked professionally in IT @ United and Sentry Insurance 🛩</h3>
-                        (3 years professional experience)
+                    <h5>(3 years professional experience)</h5>
                         <br></br><br></br>
+                        <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
                     <h3>I currently live in Steven's Point, WI - it's cold.</h3>
-                    at least the ice szn is fun
+                    <h5> at least the ice szn is fun </h5>
                     <br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
                     <h3>I am a DBA! Been avoiding select * from production since 2021</h3>
-                    id recommend using a select top(10) * instead, cheers.
-                    
-                </Jumbotron>
-                </Styles>
-            </Container>
+                    <h5> id recommend using a select top(10) [some_col(s)] instead, cheers. </h5>
+                    </div>
+    <Styles>
+    <Jumbotron className="rellax" data-rellax-speed="5" z>
+    <Container>
+    <h1 align = 'right'>🛰</h1>
+    <h1>🚀</h1>
+    </Container>
+    </Jumbotron>
+    </Styles>
+    </Styles>
+    </Container>
      
      <Scroller>
     </Scroller>
+  
+    <Styles>  
+<Jumbotron className = "jumbo">
+
+        <div>
+        <h2> Thanks for reading! check out these cool pages too:</h2>
+        <br></br><br></br>
+       <h2><a href = "https://github.com/samuelcourtney">💻 GitHub </a></h2>
+        <div>
+        <h2><a href = "https://www.linkedin.com/in/csamcourtney/">🔗 LinkedIn </a></h2>
+        </div>
+        <h2><a href = "https://www.instagram.com/samuelxjc/?hl=en">📱 Instagram</a></h2>
+
+        <div className = "Aapp">
+        <h2><a href = {resumepdf} target = "_blank">📄 Resume</a></h2>
+        </div>
+
+        <div className = "App">
+        <h2><a href = {degreepdf} target = "_blank">🎓 Degree</a></h2>
+        </div>
+
+
+
+        <h2><a href="#/about" class="button">📚 Studies</a></h2>
+        <h2><a href="#/music" class="button">🎹 Music</a></h2>
+      </div>
+
+    </Jumbotron>
+
+    <Styles>
+        <Container>
+            <Jumbotron>
+    <h4 align = 'right' className="rellax" data-rellax-speed="3">🌤</h4>
+    <h4 align = 'left' className="rellax" data-rellax-speed="3">you are here</h4>
+    <br></br>
+    <h4 className="rellax" data-rellax-speed="3">&emsp;/</h4>
+    <h4 className="rellax" data-rellax-speed="3" >🌎</h4>
+    </Jumbotron>
+    </Container>
+    </Styles>
+
+    </Styles>
     {/*
         <div className="rellax">
         I’m that default chill speed of "-2"
